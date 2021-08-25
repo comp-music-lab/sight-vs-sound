a6465271ce3a0487c7799435c4365b050d2145d5
gakuto1207 <64592680+gakuto1207@users.noreply.github.com>
2021-08-26 07:04:47 +0900

    Add files via upload

--- pilot-data/sight-vs-sound_6s.R
+++ pilot-data/sight-vs-sound_6s.R
@@ -2,7 +2,7 @@
 #Chiba, G., Savage, P. E., & Fujii, S. Sight vs. sound in the judgment of music performance: evidence from Tsugaru shamisen competitions in Japan.
 
 #Set working directory
-setwd("/Volumes/NO NAME/2021/01_SFC/spring-semester/zemi/sight-vs-sound/pilot_data")
+setwd("/Volumes/2021/01_SFC/spring-semester/zemi/sight-vs-sound_02/pilot_data")
 
 #Install and load packages
 install.packages("TOSTER")
@@ -30,10 +30,11 @@ library(ggsci)
 pwr.t.test(n =, d = 0.4, sig.level = .025, power = .95, type=c("paired"), alternative=("two.sided"))
 
 #Specify data download location
-file.data <- "/Volumes/NO NAME/2021/01_SFC/spring-semester/zemi/sight-vs-sound/pilot_data/sight-vs-sound_6s.csv"
+file.data <- "/Volumes/2021/01_SFC/spring-semester/zemi/sight-vs-sound_02/pilot_data/sight-vs-sound_6s-5.csv"
 
 #Load pilot data
 data<-read.csv(file=file.data)
+data <- transform(data, Group= factor(Group, levels = c("AudioVisual", "Audio-only", "Visual-only")))
 data$winlos<-rowMeans(data[,3:4])
 winlos <- data$winlos
 
@@ -46,7 +47,7 @@ data_summary <- function(x) {
 }
 
 #Plot data
-chart_1 <- ggplot(data, aes(x=group, y=winner, color=group)) + geom_abline(intercept=33, slope=0, linetype=2, alpha=0.8) + geom_violin() + stat_summary(fun.data=data_summary, geom ="pointrange") + geom_dotplot(binaxis='y', stackdir='center', dotsize=0.6) + ylim(0,100) + labs(title = '', x = TeX('group'), y = TeX('Selecting Actual Winner ($\\mathrm{%}$)')) + theme(plot.title = element_text(size = 12, hjust = 0.5, face = "bold"), axis.text=element_text(size=10),axis.title=element_text(size=12,face="bold"))
-chart_2 <- ggplot(data, aes(x=group, y=loser, color=group)) + geom_abline(intercept=33, slope=0, linetype=2, alpha=0.8) + geom_violin() + stat_summary(fun.data=data_summary, geom ="pointrange") + geom_dotplot(binaxis='y', stackdir='center', dotsize=0.6) + ylim(0,100) + labs(title = '', x = TeX('group'), y = TeX('Selecting Actual Loser ($\\mathrm{%}$)')) + theme(plot.title = element_text(size = 12, hjust = 0.5, face = "bold"), axis.text=element_text(size=10),axis.title=element_text(size=12,face="bold"))
+chart_1 <- ggplot(data, aes(x=Group, y=winner, color=Group)) + geom_abline(intercept=33, slope=0, linetype=2, alpha=0.8) + geom_violin() + stat_summary(fun.data=data_summary, geom ="pointrange") + geom_dotplot(binaxis='y', stackdir='center', dotsize=0.6) + ylim(0,100) + labs(title = '', x = TeX('Group'), y = TeX('Selecting Actual Winner ($\\mathrm{%}$)')) + theme(plot.title = element_text(size = 12, hjust = 0.5, face = "bold"), axis.text=element_text(size=10),axis.title=element_text(size=12,face="bold"))
+chart_2 <- ggplot(data, aes(x=Group, y=loser, color=Group)) + geom_abline(intercept=33, slope=0, linetype=2, alpha=0.8) + geom_violin() + stat_summary(fun.data=data_summary, geom ="pointrange") + geom_dotplot(binaxis='y', stackdir='center', dotsize=0.6) + ylim(0,100) + labs(title = '', x = TeX('Group'), y = TeX('Selecting Actual Loser ($\\mathrm{%}$)')) + theme(plot.title = element_text(size = 12, hjust = 0.5, face = "bold"), axis.text=element_text(size=10),axis.title=element_text(size=12,face="bold"))
 
 grid.arrange(chart_1, chart_2, ncol=2, top="6 seconds clip")
