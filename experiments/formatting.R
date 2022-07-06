8e2f25b5f2997f0cfebf45c508032b67b3ed21a8
YutoOzaki <yutoozaki@hotmail.com>
2022-07-06 17:37:31 +0900

    Add existing file

--- experiments/formatting.R
+++ experiments/formatting.R
@@ -19,7 +19,37 @@ T <- T[-ROW_INVALID, ]
 S <- read.csv("./data/stimuli.csv", header = TRUE)
 
 ###### format stimuli master data ######
-df_stimuli <- rbind(unique(data.frame(data_id = as.numeric(sapply(strsplit(substring(S[, 2], 1), "-"), function(x){return(x[[1]][1])})),
+idx_1st_AO <- substr(S[, 2], nchar(S[, 2]), nchar(S[, 2])) == "1"
+idx_2nd_AO <- substr(S[, 2], nchar(S[, 2]), nchar(S[, 2])) == "2"
+idx_1st_VO <- substr(S[, 3], nchar(S[, 3]), nchar(S[, 3])) == "1"
+idx_2nd_VO <- substr(S[, 3], nchar(S[, 3]), nchar(S[, 3])) == "2"
+idx_1st_AV <- substr(S[, 4], nchar(S[, 4]), nchar(S[, 4])) == "1"
+idx_2nd_AV <- substr(S[, 4], nchar(S[, 4]), nchar(S[, 4])) == "2"
+
+if(!all(sapply(strsplit(substring(S[idx_1st_AO, 2], 1), "-"), function(x){return(x[[1]][1])}) == sapply(strsplit(substring(S[idx_2nd_AO, 2], 1), "-"), function(x){return(x[[1]][1])}))) {
+  fprintf("Data format (AO) is not valid");
+  return()
+}
+if(!all(sapply(strsplit(substring(S[idx_1st_VO, 3], 1), "-"), function(x){return(x[[1]][1])}) == sapply(strsplit(substring(S[idx_2nd_VO, 3], 1), "-"), function(x){return(x[[1]][1])}))) {
+  fprintf("Data format (VO) is not valid");
+  return()
+}
+if(!all(sapply(strsplit(substring(S[idx_1st_AV, 4], 1), "-"), function(x){return(x[[1]][1])}) == sapply(strsplit(substring(S[idx_2nd_AV, 4], 1), "-"), function(x){return(x[[1]][1])}))) {
+  fprintf("Data format (AV) is not valid");
+  return()
+}
+  
+df_performer <- rbind(
+  data.frame(data_id = as.numeric(sapply(strsplit(substring(S[idx_1st_AO, 2], 1), "-"), function(x){return(x[[1]][1])})),
+             performer_1 = S[idx_1st_AO, 8], performer_2 = S[idx_2nd_AO, 8]),
+  data.frame(data_id = as.numeric(sapply(strsplit(substring(S[idx_1st_VO, 3], 1), "-"), function(x){return(x[[1]][1])})),
+             performer_1 = S[idx_1st_VO, 8], performer_2 = S[idx_2nd_VO, 8]),
+  data.frame(data_id = as.numeric(sapply(strsplit(substring(S[idx_1st_AV, 4], 1), "-"), function(x){return(x[[1]][1])})),
+             performer_1 = S[idx_1st_AV, 8], performer_2 = S[idx_2nd_AV, 8])
+)
+
+df_stimuli <- rbind(
+            unique(data.frame(data_id = as.numeric(sapply(strsplit(substring(S[, 2], 1), "-"), function(x){return(x[[1]][1])})),
                               instrument = S[, 9],
                               varcond = S[, 11],
                               domain = "Audio-only",
@@ -47,7 +77,9 @@ if(!all(sort(unique(df_stimuli$data_id)) == (1:NUM_STIMULI))) {
 ###### organize Google form data ######
 df_data <- data.frame(participant_id = as.numeric(), data_id = as.numeric(), order = as.character(),
                       answer = as.numeric(), score = as.numeric(),
-                      instrument = as.character(), domain = as.character(), varcond = as.character(), competitoin = as.character())
+                      instrument = as.character(), domain = as.character(),
+                      varcond = as.character(), competitoin = as.character(),
+                      performer_1 = as.character(), performer_2 = as.character())
 
 NUM_PARTICIPANT <- nrow(T)
 id_i <- vector(mode = "numeric", length = NUM_DATA)
@@ -90,13 +122,15 @@ for(i in 1:NUM_PARTICIPANT) {
   answer_i <- sapply(answer_i, function(x) {return(str_replace(x, "“ñl–Ú‚Ì‰‰‘t", "2"))})
   
   df_i <- data.frame(participant_id = i, data_id = id_i, order = order_i, answer = as.numeric(answer_i), score = 0,
-                     instrument = "", domain = "", varcond = "", competition = "")
+                     instrument = "", domain = "", varcond = "", competition = "", performer_1 = "", performer_2 = "")
   df_i[df_i$order == "a" & df_i$answer == 1, ]$score <- 1
   df_i[df_i$order == "b" & df_i$answer == 2, ]$score <- 1
   df_i$instrument <- df_stimuli$instrument[df_i$data_id]
   df_i$domain <- df_stimuli$domain[df_i$data_id]
   df_i$varcond <- df_stimuli$varcond[df_i$data_id]
   df_i$competition <- df_stimuli$competition[df_i$data_id]
+  df_i$performer_1 <- df_performer$performer_1[df_i$data_id]
+  df_i$performer_2 <- df_performer$performer_2[df_i$data_id]
   
   df_i <- df_i[!(df_i$data_id %in% DATAID_INVALID), ]
   
@@ -107,7 +141,9 @@ for (j in 1:NUM_STIMULI) {
   if(!all(df_stimuli$domain[df_stimuli$data_id == j] == df_data$domain[df_data$data_id == j]) ||
      !all(df_stimuli$varcond[df_stimuli$data_id == j] == df_data$varcond[df_data$data_id == j]) ||
      !all(df_stimuli$instrument[df_stimuli$data_id == j] == df_data$instrument[df_data$data_id == j]) ||
-     !all(df_stimuli$competition[df_stimuli$data_id == j] == df_data$competition[df_data$data_id == j])) {
+     !all(df_stimuli$competition[df_stimuli$data_id == j] == df_data$competition[df_data$data_id == j]) ||
+     !all(df_stimuli$performer_1[df_stimuli$data_id == j] == df_performer$performer_1[df_data$data_id == j]) ||
+     !all(df_stimuli$performer_2[df_stimuli$data_id == j] == df_performer$performer_2[df_data$data_id == j])) {
     print("df_data data frame is not valid");
     return()
   }
--- experiments/plot_data.R
+++ experiments/plot_data.R
@@ -5,7 +5,7 @@ library(RColorBrewer)
 
 ###### Configuration ######
 INPUT_FILENAME <- "datatable_20220701.csv"
-OUTPUT_FILEID <- c("analysis_20220701_raw", "analysis_20220701_diff")
+OUTPUT_FILEID <- c("analysis_20220701_raw", "analysis_20220701_diff", "analysis_20220701_compe", "analysis_20220701_pair")
 
 G_VIOLIN_ADJUST <- 0.6
 G_VIOLIN_SCALE <- "area"
@@ -44,7 +44,37 @@ df_diff <- aggregate(temp$score, by = list(temp$participant_id, temp$instrument,
 
 names(df_diff) <- c("participant_id", "Instrument", "Variance", "score")
 
-###### Create ggplot objects ######
+###### [Figure 3] Data formatting by summarizing by competitions ######
+df_competition <- aggregate(df_data$score, by = list(df_data$competition, df_data$domain, df_data$varcond), FUN = mean)
+
+names(df_competition) <- c("Competition", "Domain", "Variance", "score")
+
+###### [Figure 4] Data formatting by summarizing by performer pairs ######
+tmp <- df_data
+tmp$performer_pair <- paste(tmp$performer_1, ' - ', tmp$performer_2, sep = "")
+df_pair <- aggregate(tmp$score, by = list(tmp$performer_pair, tmp$domain, tmp$varcond), FUN = mean)
+
+names(df_pair) <- c("Pair", "Domain", "Variance", "score")
+
+df_PERFORMER_PAIR <- unique(cbind(df_data[, 6], df_data[, 10:11]))
+df_PERFORMER_PAIR$abbrv <- "(p)"
+df_PERFORMER_PAIR$abbrv[df_PERFORMER_PAIR[, 1] == "Tsugaru shamisen"] <- "(t-s)"
+
+df_PERFORMER_PAIR <- df_PERFORMER_PAIR[sort(df_PERFORMER_PAIR$abbrv, decreasing = FALSE, index=TRUE)$ix, ]
+
+PERFORMER_PAIR <- paste(
+  df_PERFORMER_PAIR[, 2], ' - ', df_PERFORMER_PAIR[, 3], sep = ""
+)
+PERFORMER_PAIR_INI <- paste(
+  sapply(strsplit(df_PERFORMER_PAIR[, 2], " "), function(x){return(paste(substring(x[1], 1, 1), ".", substring(x[2], 1, 1), ".", sep = ""))}),
+  "\nvs\n",
+  sapply(strsplit(df_PERFORMER_PAIR[, 3], " "), function(x){return(paste(substring(x[1], 1, 1), ".", substring(x[2], 1, 1), ".", sep = ""))}),
+  "\n",
+  df_PERFORMER_PAIR$abbrv,
+  sep = ""
+)
+
+###### [Figure 1 & 2] Create ggplot objects ######
 df_list = list(df_stats, df_diff)
 g_list <- vector(mode = "list", length = 2)
 INSTRUMENT <- sort(unique(df_stats$Instrument))
@@ -80,15 +110,61 @@ for (j in 1:2) {
       ylim(G_YL[[j]]) + 
       stat_summary(fun = "mean", geom = "point", shape = 23, size = 2., fill = "black") +
       labs(x = "", y = "Percent correct", title = INSTRUMENT[i]) +
-      theme(legend.position = "none", plot.title = element_text(hjust = 0.5), axis.text.x = element_text(size = G_X_SIZE, angle = G_X_ROTATE),
-            axis.text.y = element_text(size = G_Y_SIZE), axis.title.y = element_text(size = G_Y_SIZE)) + 
+      theme(legend.position = "none",
+            plot.title = element_text(hjust = 0.5),
+            axis.text.x = element_text(size = G_X_SIZE, angle = G_X_ROTATE),
+            axis.text.y = element_text(size = G_Y_SIZE),
+            axis.title.y = element_text(size = G_Y_SIZE)
+            ) + 
       scale_x_discrete(limits = G_XLIMITS[[j]], labels = G_XLABELS[[j]])
   }
   
   ###### Merge ggplot objects ######
   g <- grid.arrange(grobs = g_list, nrow = 2)
   
-  ###### [Figure 1] Save the figure ######
+  ###### Save the figure ######
   ggsave(file = paste("./output/", OUTPUT_FILEID[j], "_ggplots.png", sep = ""),
+         plot = g, width = G_WID, height = G_HEI)
+}
+
+###### [Figure 3 & 4] Create ggplot objects ######
+for (j in 1:2) {
+  if (j == 1) {
+    g <- ggplot(data = df_competition,
+                          aes(x = Competition, y = score, colour = Domain, shape = Variance))
+    G_TITLESTR <- "Competition-wise summary"
+  }else if (j == 2) {
+    g <- ggplot(data = df_pair,
+                          aes(x = Pair, y = score, colour = Domain, shape = Variance))
+    G_TITLESTR <- "Performer pair-wise summary"
+  }
+  
+  g <- g +
+    geom_jitter(width = G_JITTER_WID, height = 0) +
+    ylim(0, 1) +
+    labs(x = "", y = "Percent correct", title = G_TITLESTR) +
+    theme(plot.title = element_text(hjust = 0.5),
+          axis.text.x = element_text(size = G_X_SIZE, angle = G_X_ROTATE),
+          axis.text.y = element_text(size = G_Y_SIZE),
+          axis.title.y = element_text(size = G_Y_SIZE)
+    )
+  
+  if (j == 1) {
+    g <- g + 
+      scale_x_discrete(
+        limits = c("International Franz Liszt Piano Competition",
+                   "Van Cliburn International Piano Competition",
+                   "San Marino Piano Competition",
+                   "International Hirosaki Tsugaru shamisen Competition",
+                   "International Michinoku Tsugaru shamisen Competition",
+                   "International Biwako Tsugaru shamisen Competition"),
+        labels = c("Liszt\n(p)", "Van Cliburn\n(p)", "San Marino\n(p)", "Hirosaki\n(t-s)", "Michinoku\n(t-s)", "Biwako\n(t-s)")
+      )
+  }else if (j == 2) {
+    g <- g + 
+      scale_x_discrete(limits = PERFORMER_PAIR, labels = PERFORMER_PAIR_INI)
+  }
+  
+  ggsave(file = paste("./output/", OUTPUT_FILEID[2 + j], "_ggplots.png", sep = ""),
          plot = g, width = G_WID, height = G_HEI)
 }
